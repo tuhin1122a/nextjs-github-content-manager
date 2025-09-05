d35ce1da2765070729d93cdea83744099566efff
Tuhinur Rahman <tuhinrahmna48@gmail.com>
2025-09-05 11:36:39 +0600

    create next js app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbo: {
+      root: "C:/Users/C Z/Desktop/job-task", // তোমার project root path
+    },
+  },
 };
 
 export default nextConfig;
